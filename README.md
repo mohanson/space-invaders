738ee8aef2386c4165ca5bc95a01df0741833875
mohanson <mohanson@outlook.com>
2019-05-22 20:18:20 +0800

    Full implemention

---
+++ README.md
@@ -0,0 +1,33 @@
+# Space-Invaders
+
+Space Invaders (Japanese: スペースインベーダー Hepburn: Supēsu Inbēdā) is a 1978 arcade game created by Tomohiro Nishikado. It was manufactured and sold by Taito in Japan, and licensed in the United States by the Midway division of Bally. Within the shooter genre, Space Invaders was the first fixed shooter and set the template for the shoot 'em up genre. The goal is to defeat wave after wave of descending aliens with a horizontally moving laser to earn as many points as possible.
+
+```sh
+$ cargo run
+```
+
+- Press `C` to insert a coin
+- Press `1` to start player 1
+- Press `Q` to move left
+- Press `E` to move right
+- Press `W` to fire
+
+The game uses an **Intel 8080 central processing unit (CPU)**, displays raster graphics on a CRT monitor, and uses monaural sound hosted by a combination of analog circuitry and a Texas Instruments SN76477 sound chip.
+
+![img](./res/space-invaders.gif)
+
+I use a separate repo to implement the i8080 CPU, please goto [https://github.com/mohanson/i8080](https://github.com/mohanson/i8080) to learn more.
+
+# Tests
+
+Tested in the following environments:
+
+- Rust 1.34.0
+
+# Specs
+
+- [http://computerarcheology.com/Arcade/SpaceInvaders/Hardware.html](http://computerarcheology.com/Arcade/SpaceInvaders/Hardware.html)
+
+# Licences
+
+WTFPL
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,282 @@
-use std::io::Read;
+use std::cell::RefCell;
+use std::fs::File;
+use std::io::{Cursor, Read};
+use std::path::Path;
+use std::rc::Rc;
 
-fn main() {
-    let mut mem = i8080::Linear::new();
-    let mut file = std::fs::File::open("/src/i8080/res/cpu_tests/8080PRE.COM").unwrap();
-    let mut buf = Vec::new();
-    file.read_to_end(&mut buf).unwrap();
-    mem.data[0x0100..(buf.len() + 0x0100)].clone_from_slice(&buf[..]);
+use i8080::Memory;
 
-    let mut cpu = i8080::Cpu::power_up(Box::new(mem));
+#[derive(Default)]
+struct DeviceIO {
+    inp0: u8,       // INPUTS (Mapped in hardware but never used by the code)
+    inp1: u8,       // INPUTS
+    inp2: u8,       // INPUTS
+    shft_in: u8,    // bit shift register read
+    shft_amnt: u8,  // shift amount (3 bits)
+    sound1: u8,     // sound bits
+    shft_data: u16, // shift data
+    sound2: u8,     // sound bits
+    watchdog: u8,   // watch-dog
+}
 
-    cpu.mem.set(0x0005, 0xc9);
-    cpu.reg.pc = 0x0100;
+impl DeviceIO {
+    fn power_up() -> Self {
+        Self::default()
+    }
+}
 
-    loop {
-        cpu.next();
-        if cpu.reg.pc == 0x76 {
-            panic!("")
-        }
-        if cpu.reg.pc == 0x05 {
-            if cpu.reg.c == 0x09 {
-                let mut a = cpu.reg.get_de();
-                loop {
-                    let c = cpu.mem.get(a);
-                    if c as char == '$' {
-                        break;
-                    } else {
-                        a += 1;
-                    }
-                    print!("{}", c as char);
-                }
-            }
-            if cpu.reg.c == 0x02 {
-                print!("{}", cpu.reg.e as char);
-            }
-        }
-        if cpu.reg.pc == 0x00 {
-            println!("");
-            println!("");
-            break;
+const DISPLAY_W: usize = 224;
+const DISPLAY_H: usize = 256;
+
+struct Display {
+    raster: Vec<u32>,
+    window: minifb::Window,
+}
+
+impl Display {
+    fn power_up() -> Display {
+        let mut option = minifb::WindowOptions::default();
+        option.resize = true;
+        option.scale = minifb::Scale::X2;
+        let window = minifb::Window::new("github.com/mohanson/i8080", DISPLAY_W, DISPLAY_H, option).unwrap();
+        Display {
+            raster: vec![0x00ff_ffff; DISPLAY_W * DISPLAY_H * 2],
+            window,
         }
     }
 
+    fn draw_pixel(&mut self, data: &[u8]) {
+        for (i, byte) in data.iter().enumerate() {
+            let y = i as isize * 8 / 256;
+
+            for shift in 0..8 {
+                let x = ((i * 8) % 256 as usize + shift as usize) as isize;
+
+                // Rotate frame buffer 90 deg
+                let new_x = y as isize;
+                let new_y = (-x as isize + 256) - 1;
+
+                let pixel = if byte.wrapping_shr(shift) & 1 == 0 {
+                    0xFF00_0000 // Alpha
+                } else if x <= 63 && (x >= 15 || x <= 15 && y >= 20 && y <= 120) {
+                    0xFF00_FF00 // Green
+                } else if x >= 200 && x <= 220 {
+                    0xFF00_00FF // Red
+                } else {
+                    0xFFFF_FFFF // Black
+                };
+                self.raster[DISPLAY_W * new_y as usize + new_x as usize] = pixel;
+            }
+        }
+        self.window.update_with_buffer(&self.raster).unwrap();
+    }
+}
+
+struct Sounder {
+    sink: rodio::Sink,
+    wavs: [Vec<u8>; 10],
+}
+
+impl Sounder {
+    fn power_up(snd: impl AsRef<Path>) -> Self {
+        let res = snd.as_ref().to_path_buf();
+        let get = |x| {
+            let mut res = res.clone();
+            res.push(x);
+            std::fs::read(res).unwrap()
+        };
+        let mut wavs = [
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+        ];
+        for i in 0..10 {
+            wavs[i as usize] = get(format!("{}.wav", i));
+        }
+        let device = rodio::default_output_device().unwrap();
+        let sink = rodio::Sink::new(&device);
+        Self { sink, wavs }
+    }
+
+    fn play_sound(&self, i: usize) {
+        let data = self.wavs[i].clone();
+        let cursor = Cursor::new(data);
+        self.sink.append(rodio::Decoder::new(cursor).unwrap());
+    }
+}
+
+struct Invaders {
+    cpu: i8080::Cpu,
+    display: Display,
+    sounder: Sounder,
+    interrupt_addr: u16,
+    mem: Rc<RefCell<i8080::Linear>>,
+    io: DeviceIO,
+}
+
+impl Invaders {
+    fn power_up(rom: impl AsRef<Path>) -> Self {
+        let mem = Rc::new(RefCell::new(i8080::Linear::new()));
+        let mut file = File::open(rom).unwrap();
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf).unwrap();
+        mem.borrow_mut().data[0x00..buf.len()].clone_from_slice(&buf[..]);
+
+        Self {
+            cpu: i8080::Cpu::power_up(mem.clone()),
+            display: Display::power_up(),
+            sounder: Sounder::power_up("./res/snd"),
+            interrupt_addr: 0x08,
+            mem,
+            io: DeviceIO::power_up(),
+        }
+    }
+
+    fn play_sound(&mut self, data: u8, bank: u8) {
+        if bank == 1 && data != self.io.sound1 {
+            if i8080::bit::get(data, 0) && !i8080::bit::get(self.io.sound1, 0) {
+                self.sounder.play_sound(0)
+            }
+            if i8080::bit::get(data, 1) && !i8080::bit::get(self.io.sound1, 1) {
+                self.sounder.play_sound(1)
+            }
+            if i8080::bit::get(data, 2) && !i8080::bit::get(self.io.sound1, 2) {
+                self.sounder.play_sound(2)
+            }
+            if i8080::bit::get(data, 3) && !i8080::bit::get(self.io.sound1, 3) {
+                self.sounder.play_sound(3)
+            }
+            self.io.sound1 = data;
+        }
+        if bank == 2 && data != self.io.sound2 {
+            if i8080::bit::get(data, 0) && !i8080::bit::get(self.io.sound2, 0) {
+                self.sounder.play_sound(4)
+            }
+            if i8080::bit::get(data, 1) && !i8080::bit::get(self.io.sound2, 1) {
+                self.sounder.play_sound(5)
+            }
+            if i8080::bit::get(data, 2) && !i8080::bit::get(self.io.sound2, 2) {
+                self.sounder.play_sound(6)
+            }
+            if i8080::bit::get(data, 3) && !i8080::bit::get(self.io.sound2, 3) {
+                self.sounder.play_sound(7)
+            }
+            if i8080::bit::get(data, 4) && !i8080::bit::get(self.io.sound2, 4) {
+                self.sounder.play_sound(8)
+            }
+            self.io.sound2 = data;
+        }
+    }
+
+    fn handle_joypad(&mut self) {
+        if self.display.window.is_key_down(minifb::Key::Escape) {
+            std::process::exit(0);
+        }
+        let keys = vec![
+            (minifb::Key::C, 1, 0),     // Insert a coin
+            (minifb::Key::Key2, 1, 1),  // P2 start
+            (minifb::Key::Key1, 1, 2),  // P1 start
+            (minifb::Key::W, 1, 4),     // P1 shot
+            (minifb::Key::Up, 2, 4),    // P2 shot
+            (minifb::Key::Q, 1, 5),     // P1 left
+            (minifb::Key::Left, 2, 5),  // P2 left
+            (minifb::Key::E, 1, 6),     // P1 right
+            (minifb::Key::Right, 2, 6), // p2 right
+            (minifb::Key::T, 2, 2),     // tilt
+        ];
+        for (k, inpi, biti) in &keys {
+            let func = |x| {
+                if self.display.window.is_key_down(*k) {
+                    i8080::bit::set(x, *biti as usize)
+                } else {
+                    i8080::bit::clr(x, *biti as usize)
+                }
+            };
+            match inpi {
+                1 => self.io.inp1 = func(self.io.inp1),
+                2 => self.io.inp2 = func(self.io.inp2),
+                _ => {}
+            }
+        }
+    }
+
+    fn next(&mut self) -> u32 {
+        let opcode = self.mem.borrow().get(self.cpu.reg.pc);
+        match opcode {
+            0xdb => {
+                let port = self.mem.borrow().get(self.cpu.reg.pc + 1);
+                let r = match port {
+                    0 => self.io.inp0,
+                    1 => self.io.inp1,
+                    2 => self.io.inp2,
+                    3 => {
+                        self.io.shft_in = ((self.io.shft_data >> u16::from(8 - self.io.shft_amnt)) & 0xff) as u8;
+                        self.io.shft_in
+                    }
+                    _ => panic!(""),
+                };
+                self.cpu.reg.a = r;
+            }
+            0xd3 => {
+                let port = self.mem.borrow().get(self.cpu.reg.pc + 1);
+                match port {
+                    2 => self.io.shft_amnt = self.cpu.reg.a & 0x7,
+                    3 => {
+                        self.play_sound(self.cpu.reg.a, 1);
+                    }
+                    4 => self.io.shft_data = u16::from(self.cpu.reg.a) << 8 | self.io.shft_data >> 8,
+                    5 => {
+                        self.play_sound(self.cpu.reg.a, 2);
+                    }
+                    6 => self.io.watchdog = self.cpu.reg.a,
+                    7 => {}
+                    _ => panic!(""),
+                }
+            }
+            _ => {}
+        }
+        self.cpu.step()
+    }
+
+    fn step(&mut self) {
+        let mut cycle = 0;
+        while cycle < 16667 {
+            cycle += self.next();
+        }
+        self.cpu.inte_handle(self.interrupt_addr);
+        self.interrupt_addr = if self.interrupt_addr == 0x08 { 0x10 } else { 0x08 };
+        while cycle < 33334 {
+            cycle += self.next();
+        }
+        self.display.draw_pixel(&self.mem.borrow().data[0x2400..0x4000]);
+        self.handle_joypad();
+    }
+}
+
+fn main() {
+    let mut invaders = Invaders::power_up("./res/invaders.rom");
+    println!("----------------------------------");
+    println!("| Welcome to space invaders!     |");
+    println!("|                                |");
+    println!("|    Press `C` to insert a coin  |");
+    println!("|    Press `1` to start player 1 |");
+    println!("|    Press `Q` to move left      |");
+    println!("|    Press `E` to move right     |");
+    println!("|    Press `W` to fire           |");
+    println!("|                                |");
+    println!("| Good luck                      |");
+    println!("----------------------------------");
+    loop {
+        if !invaders.display.window.is_open() {
+            return;
+        }
+        invaders.step();
+    }
 }
